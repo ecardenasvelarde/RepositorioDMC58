9434850ec8c7dc23ae3bc2183c31b948620f2d93
ecardenasvelarde <e.cardenas.velarde@gmail.com>
2026-05-12 19:46:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,6 @@ st.title("Mi primera aplicación en python")
 
 st.sidebar.title("Parámetros")
 
-st.write("Elaborado por: Carlos Carrillo")
+st.write("Elaborado por: Erick Cardenas")
+
+sesion = st.selectbox("Seleccione una sesión",["Sesión 1","Sesión 2","Sesión 3","Sesión 4"])
