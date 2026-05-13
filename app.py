3130bfc4b59991ffd4c04b6846b3a76a1ff8e254
ecardenasvelarde <e.cardenas.velarde@gmail.com>
2026-05-12 19:50:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ st.sidebar.title("Parámetros")
 
 st.write("Elaborado por: Erick Cardenas")
 
-sesion = st.selectbox("Seleccione una sesión",["Sesión 1","Sesión 2","Sesión 3","Sesión 4"])
+sesion = st.sidebar.selectbox("Seleccione una sesión",["Sesión 1","Sesión 2","Sesión 3","Sesión 4"])
